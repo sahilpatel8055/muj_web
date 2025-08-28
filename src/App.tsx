45ee93af6f6f7b00aaab5f8719166aed1d8043bd
sahilpatel8055 <Sahilpatelbs@gmail.com>
2025-08-28 23:23:34 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -25,8 +25,6 @@ import BA_SMU from './pages/BA_SMU';
 import MA_SMU from './pages/MA_SMU';
 import BCOM_SMU from './pages/BCOM_SMU';
 import MCOM_SMU from './pages/MCOM_SMU';
-import ManipalAdvantagePage from './pages/ManipalAdvantagePage'; // Assuming this is already here from previous steps
-
 const queryClient = new QueryClient();
 
 const AppContent = () => {
@@ -56,7 +54,6 @@ const AppContent = () => {
         <Route path="/ma-smu" element={<MA_SMU />} />
         <Route path="/bcom-smu" element={<BCOM_SMU />} />
         <Route path="/mcom-smu" element={<MCOM_SMU />} />
-        <Route path="/advantage" element={<ManipalAdvantagePage />} /> {/* Assuming this route is already present */}
         {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
         <Route path="*" element={<NotFound />} />
       </Routes>
